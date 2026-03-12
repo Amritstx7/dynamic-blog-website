c0f95e8351d49bebec19308edafed95132e4cf7f
Amrit <ajaibtiwana@macbookair.home>
2026-03-11 20:51:33 -0400

    Save current changes in index.js before syncing with main

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const postList = document.getElementById('post-list');
 
 let posts = JSON.parse(localStorage.getItem('posts')) || [];
 
+postList.innerHTML = '';
 
-posts.forEach(post => {
-    const li = document.createElement('li');
-    li.innerHTML = `<strong>${post.title}</strong><br>${post.content} <br><a href="post.html?id=${post.id}">View/Edit</a>`;
-    postList.appendChild(li);
-});
+
+if (posts.length === 0) {
+    postList.innerHTML = '<li>No posts yet. Create a new post!</li>';
+} else {
+ 
+    posts.forEach(post => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <strong>${post.title}</strong><br>
+            ${post.content} <br>
+            <a href="post.html?id=${post.id}">View/Edit</a>
+        `;
+        postList.appendChild(li);
+    });
+}
